cea2ab5597a15862ff7ac6e6d45f4dc9b06e6d83
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-09-23 12:28:45 +0200

    meter_advanced

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,37 +1,32 @@
-/*
-  Blink
+#define SAMPLE_NR 400
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+unsigned long times[SAMPLE_NR]; //= {0,11,22,33,44,55,66, 77, 88, 99};
+unsigned int times_iter = 0;
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
-*/
-
-// the setup function runs once when you press reset or power the board
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void Interrupt_routine(){
+  times[times_iter++] = micros();
+  if(times_iter == SAMPLE_NR){
+    detachInterrupt(digitalPinToInterrupt(2));
+  }
 }
 
-// the loop function runs over and over again forever
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.setTimeout(-1);
+
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), Interrupt_routine, RISING);
+  }
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  Serial.readStringUntil('\n');
+
+  detachInterrupt(digitalPinToInterrupt(2));
+  
+  for(unsigned int i = 0; i < SAMPLE_NR; i++){
+    Serial.println(times[i]);
+  }
+  times_iter = 0;
+  attachInterrupt(digitalPinToInterrupt(2), Interrupt_routine, RISING);
 }
