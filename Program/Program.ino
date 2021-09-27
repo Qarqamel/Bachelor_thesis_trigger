66a39ffbd1d46f34b33540de1514e7adc384a684
Adam <adam.mundala@gmail.com>
2021-09-27 15:51:00 +0200

    meter_advanced_capture

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,15 +1,15 @@
 #define SAMPLE_NR 400
 
 unsigned int captured_times[SAMPLE_NR];
-unsigned int captured_time_prev;
+//unsigned int captured_time_prev;
 unsigned int tbl_iter = 0;
 volatile bool acquisition_complete = false;
 
 ISR(TIMER1_CAPT_vect){
-  captured_times[tbl_iter++] = ICR1 - captured_time_prev;
-  captured_time_prev = ICR1;
+  captured_times[tbl_iter++] = ICR1; // - captured_time_prev;
+  //captured_time_prev = ICR1;
   if(tbl_iter == SAMPLE_NR){
-    TIMSK1 &= 0b11011111;
+    TIMSK1 &= 0b11011111; //disable timer input capture interrupts
     acquisition_complete = true;
   }
 }
@@ -17,7 +17,7 @@ ISR(TIMER1_CAPT_vect){
 void setup() {
 
   pinMode(4, INPUT); //ICP1 pin - input capture pin. pin 4 - PD4 for Leonardo; pin 8 - PB0 for Uno
-  TCCR1B = 0b01011010;
+  TCCR1B = 0b01011010; //enable timer input capture interrupts
   
   Serial.begin(115200);
   Serial.setTimeout(-1);
