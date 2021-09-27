f95aaaf135812966080c464a34ca4c229199c67e
Adam <adam.mundala@gmail.com>
2021-09-27 14:24:09 +0200

    meter_advanced

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,17 +1,19 @@
 #define SAMPLE_NR 400
 
-unsigned long times[SAMPLE_NR]; //= {0,11,22,33,44,55,66, 77, 88, 99};
+unsigned long times[SAMPLE_NR];
 unsigned int times_iter = 0;
+volatile bool acquisition_complete = false;
 
 void Interrupt_routine(){
   times[times_iter++] = micros();
   if(times_iter == SAMPLE_NR){
     detachInterrupt(digitalPinToInterrupt(2));
+    acquisition_complete = true;
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
   Serial.setTimeout(-1);
 
@@ -21,7 +23,10 @@ void setup() {
 void loop() {
   Serial.readStringUntil('\n');
   times_iter = 0;
+  acquisition_complete = false;  
   attachInterrupt(digitalPinToInterrupt(2), Interrupt_routine, RISING);
+
+  while(!acquisition_complete){}
   
   for(unsigned int i = 0; i < SAMPLE_NR; i++){
     Serial.println(times[i]);
