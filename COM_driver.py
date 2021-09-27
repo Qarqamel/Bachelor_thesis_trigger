01820137d3cf15a735a6e20de5f2750c44eff25d
Adam <adam.mundala@gmail.com>
2021-09-27 13:09:58 +0200

    backup

--- COM_driver.py
+++ COM_driver.py
@@ -1,12 +1,12 @@
 import serial
 from tabulate import tabulate 
 
-with serial.Serial(port="COM20", baudrate=115200,bytesize = 8,
+with serial.Serial(port="COM14", baudrate=115200,bytesize = 8,
                     timeout = None, stopbits=serial.STOPBITS_ONE) as serialPort:
 
     serialPort.write('start\n'.encode('utf-8'))
     
-    output_list = [serialPort.readline().decode('utf-8').strip() for i in range(470)]
+    output_list = [serialPort.readline().decode('utf-8').strip() for i in range(400)]
     
     output_diff_list = [int(j)-int(i) for i, j in zip(output_list, output_list[1:])]
     output_diff2_list = [int(j)-int(i) for i, j in zip(output_diff_list, output_diff_list[1:])]
--- Program/Program.ino
+++ Program/Program.ino
@@ -1,33 +1,38 @@
-#define SAMPLE_NR 470
+#define SAMPLE_NR 400
 
 unsigned int captured_times[SAMPLE_NR];
 unsigned int captured_time_prev;
 unsigned int tbl_iter = 0;
+//bool meter_active_flag = false;
 
 ISR(TIMER1_CAPT_vect){
   captured_times[tbl_iter++] = ICR1 - captured_time_prev;
   captured_time_prev = ICR1;
   if(tbl_iter == SAMPLE_NR)
-    tbl_iter = 0;
+    TIMSK1 &= 0b11011111;
+    //meter_active_flag = false;
 }
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(4, INPUT); //ICP1 pin - input capture pin. pin 4 - PD4 for Leonardo; pin 8 - PB0 for Uno
   TCCR1B = 0b01011010;
-  TIMSK1 |= 0b00100000;
-  SREG |= 0b10000000;
   
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.setTimeout(-1);
 }
 
 void loop() {
 
   Serial.readStringUntil('\n');
-  TIMSK1 &= 0b11011111;
+  //meter_active_flag = true;
+  tbl_iter = 0;
+  TIMSK1 |= 0b00100000;
+
+  //while(meter_active_flag){}
+  while(TIMSK1&0b00100000 == 0b00100000){}
+
   for(unsigned int i = 0; i < SAMPLE_NR; i++){
     Serial.println(captured_times[i]);
   }
-  TIMSK1 |= 0b00100000;
 }
