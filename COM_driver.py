bfe3147871dbd6a570639b03ef89b70b696c507a
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-09-27 11:27:45 +0200

    meter_advanced_capture_backup

---
+++ COM_driver.py
@@ -0,0 +1,20 @@
+import serial
+from tabulate import tabulate 
+
+with serial.Serial(port="COM20", baudrate=115200,bytesize = 8,
+                    timeout = None, stopbits=serial.STOPBITS_ONE) as serialPort:
+
+    serialPort.write('start\n'.encode('utf-8'))
+    
+    output_list = [serialPort.readline().decode('utf-8').strip() for i in range(470)]
+    
+    output_diff_list = [int(j)-int(i) for i, j in zip(output_list, output_list[1:])]
+    output_diff2_list = [int(j)-int(i) for i, j in zip(output_diff_list, output_diff_list[1:])]
+    
+print(tabulate(zip(output_list, output_diff_list, output_diff2_list),
+      headers = ["Timer_ctr","diff1","diff2"]))
+
+with open('outputs.txt', 'w') as f:
+    f.write(tabulate(zip(output_list, output_diff_list, output_diff2_list),
+      headers = ["Timer_ctr","diff1","diff2"]))
+    
