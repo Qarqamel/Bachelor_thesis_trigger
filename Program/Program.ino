0caf377ce71c9974f2bb9781998b68129e5016a4
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-09-28 09:46:42 +0200

    measure_period

--- Program/Program.ino
+++ Program/Program.ino
@@ -1,16 +1,18 @@
 #define SAMPLE_NR 400
-#define ICIE1_bm (1<<ICIE1)
+
+#define ENABLE_INPUT_CAPTURE() (TIMSK1 |= (1<<ICIE1))
+#define DISABLE_INPUT_CAPTURE() (TIMSK1 &= ~(1<<ICIE1))
 
 unsigned int captured_times[SAMPLE_NR];
 unsigned int captured_time_prev;
-unsigned int tbl_iter = 0;
+unsigned int sample_ctr = 0;
 volatile bool acquisition_complete = false;
 
 ISR(TIMER1_CAPT_vect){
-  captured_times[tbl_iter++] = ICR1 - captured_time_prev;
+  captured_times[sample_ctr++] = ICR1 - captured_time_prev;
   captured_time_prev = ICR1;
-  if(tbl_iter == SAMPLE_NR){
-    TIMSK1 &= ~ICIE1_bm; //disable timer input capture interrupts
+  if(sample_ctr == SAMPLE_NR){
+    DISABLE_INPUT_CAPTURE();
     acquisition_complete = true;
   }
 }
@@ -18,7 +20,7 @@ ISR(TIMER1_CAPT_vect){
 void setup() {
 
   pinMode(4, INPUT); //ICP1 pin - input capture pin. pin 4 - PD4 for Leonardo; pin 8 - PB0 for Uno
-  TCCR1B = 0b01011010; //enable timer input capture interrupts
+  TCCR1B = 0b01011010; //selecting edge for input capture, setting timer mode to CTC, and prescaler to 8
   
   Serial.begin(115200);
   Serial.setTimeout(-1);
@@ -27,13 +29,11 @@ void setup() {
 void loop() {
 
   Serial.readStringUntil('\n');
-  tbl_iter = 0;
+  sample_ctr = 0;
   acquisition_complete = false;
-  TIMSK1 |= ICIE1_bm;
+  ENABLE_INPUT_CAPTURE();
 
   while(!acquisition_complete){}
-  
-  while(TIMSK1&ICIE1_bm == ICIE1_bm){}
 
   for(unsigned int i = 0; i < SAMPLE_NR; i++){
     Serial.println(captured_times[i]);
