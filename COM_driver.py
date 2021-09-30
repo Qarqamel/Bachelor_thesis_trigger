49f096554b5a7f9952171b99e20799bb484c9e91
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-09-30 17:24:33 +0200

    measure_period_Mega

--- COM_driver.py
+++ COM_driver.py
@@ -1,20 +1,22 @@
 import serial
-from tabulate import tabulate 
+from tabulate import tabulate
 
-with serial.Serial(port="COM14", baudrate=115200,bytesize = 8,
+with serial.Serial(port="COM38", baudrate=115200, bytesize = 8,
                     timeout = None, stopbits=serial.STOPBITS_ONE) as serialPort:
-
+    
+    serialPort.readline()
+    
     serialPort.write('start\n'.encode('utf-8'))
     
-    output_list = [serialPort.readline().decode('utf-8').strip() for i in range(400)]
+    output_list = [serialPort.readline().decode('utf-8').strip() for i in range(2900)]
+    print(output_list)
     
-    output_diff_list = [int(j)-int(i) for i, j in zip(output_list, output_list[1:])]
-    output_diff2_list = [int(j)-int(i) for i, j in zip(output_diff_list, output_diff_list[1:])]
+output_diff_list = [int(j)-int(i) for i, j in zip(output_list, output_list[1:])]
     
-print(tabulate(zip(output_list, output_diff_list, output_diff2_list),
-      headers = ["Timer_ctr","diff1","diff2"]))
+print(tabulate(zip(output_list, output_diff_list),
+      headers = ["Timer_ctr","diff1"]))
 
 with open('outputs.txt', 'w') as f:
-    f.write(tabulate(zip(output_list, output_diff_list, output_diff2_list),
-      headers = ["Timer_ctr","diff1","diff2"]))
+    f.write(tabulate(zip(output_list, output_diff_list),
+      headers = ["Period","diff1"]))
     
--- Program/Program.ino
+++ Program/Program.ino
@@ -1,31 +1,22 @@
-//MEASURE_PERIOD
-//
-//Mierzy okres PWM-a wejściowego SAMPLE_NR razy i wysyła wyniki przez UART.
-//Pomiar rozpoczyna się po odebraniu na UART-cie dowolnej wiadomości zakończonej terminatorem ('\n').
-//Okres wejściowy: 0-32767us
-//
-//INPUT: 8-PB0(Uno), 4-PD4(Leonardo), 49-PL0(Mega)
-//OUTPUT: -
-//
-//Program korzysta z funkcji input capture 16-bitowego timera timer1(Uno, Leonardo). Timer działa w trybie CTC(Uno, Leonardo) z preskalerem ustawionym na 8.
-//Po otrzymaniu terminatora na UART-cie program rozpoczyna pomiar.
-//Na zboczach rosnących przebiegu wejściowego, wpisuje czas od poprzedniego zbocza rsonącego, do tablicy wyników.
-//Po wypełnieniu wysyła tablicę wyników przez UART.
+#define SAMPLE_NR 2900
 
-#define SAMPLE_NR 400
+#define ENABLE_INPUT_CAPTURE() (TIMSK4 |= (1<<ICIE4))
+#define DISABLE_INPUT_CAPTURE() (TIMSK4 &= ~(1<<ICIE4))
 
-#define ENABLE_INPUT_CAPTURE() (TIMSK1 |= (1<<ICIE1))
-#define DISABLE_INPUT_CAPTURE() (TIMSK1 &= ~(1<<ICIE1))
-
-uint16_t captured_times[SAMPLE_NR];
-uint16_t captured_time_prev;
-unsigned int sample_ctr = 0;
+volatile unsigned int Periods[SAMPLE_NR];
+volatile unsigned int Period_prev;
+volatile unsigned int edge_ctr = 0;
 volatile bool acquisition_complete = false;
+volatile bool first_acq = false;
 
-ISR(TIMER1_CAPT_vect){
-  captured_times[sample_ctr++] = ICR1 - captured_time_prev; //poniewaz int an ATMEGA ma 16bitów - nie jest konieczna obsługa przekręcania licznika timera
-  captured_time_prev = ICR1;
-  if(sample_ctr == SAMPLE_NR){
+ISR(TIMER4_CAPT_vect){
+  Periods[edge_ctr++] = ICR4 - Period_prev;
+  Period_prev = ICR4;
+  if(first_acq){
+    edge_ctr--;
+    first_acq = false;
+  }
+  if(edge_ctr == SAMPLE_NR){
     DISABLE_INPUT_CAPTURE();
     acquisition_complete = true;
   }
@@ -33,23 +24,32 @@ ISR(TIMER1_CAPT_vect){
 
 void setup() {
 
-  pinMode(4, INPUT); //ICP1 pin - input capture pin. pin 4 - PD4 for Leonardo; pin 8 - PB0 for Uno
-  TCCR1B = 0b01011010; //selecting edge for input capture, setting timer mode to CTC, and prescaler to 8
+  pinMode(8, INPUT); //ICP1 pin - input capture pin. pin 4 - PD4 for Leonardo; pin 8 - PB0 for Uno;ICP4 pin 49 - PL1 for Mega
+  
+  //ICES - input capture edge select to rising, CS - setting prescaler to 8
+  //WGM - setting waveform generation mode to 0
+  TCCR4B |= (1<<ICES4)|(1<<CS41);
+  TCCR4B &= ~((1<<ICNC4)|(1<<WGM43)|(1<<WGM42)|(1<<CS42)|(1<<CS40));
+  TCCR4A &= ~((1<<COM4A1)|(1<<COM4A0)|(1<<COM4B1)|(1<<COM4B0)|(1<<COM4C1)|(1<<COM4C0)|(1<<WGM41)|(1<<WGM40));
+  
   
   Serial.begin(115200);
   Serial.setTimeout(-1);
+
+  Serial.println("ready");
 }
 
 void loop() {
 
   Serial.readStringUntil('\n');
-  sample_ctr = 0;
-  acquisition_complete = false;
+  edge_ctr=0;
+  acquisition_complete=false;
+  first_acq = true;
   ENABLE_INPUT_CAPTURE();
 
   while(!acquisition_complete){}
 
-  for(unsigned int i = 0; i < SAMPLE_NR; i++){
-    Serial.println(captured_times[i]);
+  for(unsigned int i=0; i<SAMPLE_NR; i++){
+    Serial.println(Periods[i]);
   }
 }
