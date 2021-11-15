6b2baae3ff3131784c4eef78f8366442156970d0
Adam <adam.mundala@gmail.com>
2021-11-15 20:22:47 +0100

    TB_signal_processing_delay

--- Signal_processing.py
+++ Signal_processing.py
@@ -1,19 +1,26 @@
-from my_serial import my_serial_9600,read_byte,writeln
+from my_serial import my_serial,read_byte,writeln,read
 
-TB_COM_NR = 6
+CAMERA_COM_NR = 11
+SELECTOR_COM_NR = 3
+GENERATOR_COM_NR = 19
 
 sample_ctr = 0
 
-with my_serial_9600(TB_COM_NR) as sr_tb:
-    writeln(sr_tb, "Start")
+with my_serial(CAMERA_COM_NR) as sr_camera:
+    with my_serial(SELECTOR_COM_NR) as sr_selector:
+        with my_serial(GENERATOR_COM_NR) as sr_generator:
+            read(sr_camera)
+            read(sr_selector)
+            read(sr_generator)
+            
+            writeln(sr_generator, "1")
+            writeln(sr_camera, "Start")
     
-    last_sample = 1
-    
-    while True:
-        curr_sample = int(read_byte(sr_tb))&0b1
-        if(last_sample == 0 and curr_sample == 1):
-            writeln(sr_tb, str(sample_ctr))
-        last_sample = curr_sample
-        sample_ctr += 1
-    
-
+            last_sample = 1
+            
+            while True:
+                curr_sample = int(read_byte(sr_camera))&0b1
+                if(last_sample == 0 and curr_sample == 1):
+                    writeln(sr_selector, str(sample_ctr))
+                last_sample = curr_sample
+                sample_ctr += 1
