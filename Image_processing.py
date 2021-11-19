00b1f837a5db91af8ad030f17368b2f6c77e223e
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-11-19 19:40:55 +0100

    Update Image_processing.py

--- Image_processing.py
+++ Image_processing.py
@@ -1,16 +1,17 @@
 from my_serial import my_serial,read_byte,writeln,read
 
-TESTER_COM_NR = 11
+TESTER_COM_NR = 14
 
 sample_ctr = 0
 
 with my_serial(TESTER_COM_NR) as sr_tester:
-        read(sr_tester)
+        # read(sr_tester)
         
-        last_sample = 0
+        last_sample = 1
         
         while True:
             curr_sample = int(read_byte(sr_tester))&0b1
+            print(curr_sample)
             if(last_sample == 0 and curr_sample == 1):
                 writeln(sr_tester, str(sample_ctr))
             last_sample = curr_sample
