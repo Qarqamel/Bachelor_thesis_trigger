1232ce22b474c35b45dad80685ac63ecb2534b2d
Adam <adam.mundala@gmail.com>
2021-11-20 15:33:24 +0100

    TB_image_processing_untested

--- Image_processing.py
+++ Image_processing.py
@@ -10,7 +10,9 @@ with my_serial(TESTER_COM_NR) as sr_tester:
         last_sample = 1
         
         while True:
-            curr_sample = int(read_byte(sr_tester))&0b1
+            rcvd_byte = int(read_byte(sr_tester))&0b11
+            curr_sample = rcvd_byte&0b01
+            # stop_bit = rcvd_byte&0b10
             print(curr_sample)
             if(last_sample == 0 and curr_sample == 1):
                 writeln(sr_tester, str(sample_ctr))
