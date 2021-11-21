e06a81b40bb8215b6d5ebb995a15262c70a8cd97
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-11-21 23:18:56 +0100

    Add files via upload

--- Image_processing_tester/Image_processing_tester.ino
+++ Image_processing_tester/Image_processing_tester.ino
@@ -1,6 +1,13 @@
 #define TIMEBASE_PIN 2
 #define WAIT_TO_START_PIN 11
 #define STARTED_PIN 13
+
+#define SAMPLE_CH_bm 0
+#define STOP_CH_bm 1
+
+#define TB_sr Serial
+#define Sig_proc_sr Serial1
+
 #define PULSES_NR_PER_ACQ 100
 
 void StartupSynchronization(){
@@ -15,9 +22,9 @@ volatile unsigned long Sample_ctr = 0;
 volatile byte sample_val = 0;
 
 void OnChange_Timebase(){
-  Serial1.print(sample_val);
+  Sig_proc_sr.print(sample_val);
   Sample_ctr++;
-  if(sample_val >= 1){
+  if(sample_val>0){
     sample_val = 0;
     Sample_ctr = 0;
   }    
@@ -26,20 +33,20 @@ void OnChange_Timebase(){
 void setup() {
   pinMode(TIMEBASE_PIN, INPUT_PULLUP);
 
-  Serial1.begin(115200);
-  Serial1.setTimeout(-1);
-  Serial.begin(115200);
-  Serial.setTimeout(-1);
+  Sig_proc_sr.begin(115200);
+  Sig_proc_sr.setTimeout(-1);
+  TB_sr.begin(115200);
+  TB_sr.setTimeout(-1);
   
-  Serial.println("Started");
-  Serial.readStringUntil('\n');
+  TB_sr.println("Started");
+  TB_sr.readStringUntil('\n');
 
   StartupSynchronization();
   attachInterrupt(digitalPinToInterrupt(TIMEBASE_PIN), OnChange_Timebase, RISING);
 }
 
 void loop() {
-  sample_val = Serial.readStringUntil('\n').toInt();
-  Serial.println(Serial1.readStringUntil('\n'));
-  Serial.println(Sample_ctr);
+  sample_val = TB_sr.readStringUntil('\n').toInt();
+  TB_sr.println(Sig_proc_sr.readStringUntil('\n'));
+  TB_sr.println(Sample_ctr);
 }
