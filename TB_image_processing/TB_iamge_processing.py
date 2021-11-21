1843a95f272af7e818a8cdc786b9bd0b1eb62541
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-11-21 23:16:22 +0100

    Update TB_iamge_processing.py

--- TB_image_processing/TB_iamge_processing.py
+++ TB_image_processing/TB_iamge_processing.py
@@ -1,16 +1,29 @@
 from my_serial import my_serial,read,writeln,write
-import shutil, os
+import shutil, os, time, random, threading
 import pandas as pd
 from tabulate import tabulate
+from tqdm import tqdm
+from subprocess import call
 
 TESTER_COM_NR = 13
 GENERATOR_COM_NR = 7
 
+SAMPLE_CH_NR = 0
+STOP_CH_NR = 1
+
 PULSES_NR = 100
+
+def Signal_processing_task():
+    call(['python', 'Image_processing.py'])
+    
+Signal_processing_thread =threading.Thread(target=Signal_processing_task)
+Signal_processing_thread.start()
 
 with my_serial(TESTER_COM_NR) as sr_tester: 
     read(sr_tester)
     writeln(sr_tester, 'Start')
+
+df = pd.DataFrame(columns=['Timestamps', 'Delays [ms]'])
 
 with my_serial(GENERATOR_COM_NR) as sr_generator:    
     with my_serial(TESTER_COM_NR) as sr_tester:        
@@ -19,19 +32,15 @@ with my_serial(GENERATOR_COM_NR) as sr_generator:
         
         writeln(sr_generator, '1')
         writeln(sr_tester, 'Start')
-        
-        delays = []
-        timestamps = []
-        for i in range(PULSES_NR):
-            writeln(sr_tester, str(0b1))
-            timestamps.append(int(read(sr_tester)))
-            delays.append(int(read(sr_tester)))
-        writeln(sr_tester, str(0b10))
-                
-df = pd.DataFrame()
-df['Timestamps'] = timestamps
-df['Delays [ms]'] = delays
 
+        for i in tqdm(range(PULSES_NR)):
+            writeln(sr_tester, str(1<<SAMPLE_CH_NR))
+            new_row = {'Timestamps':int(read(sr_tester)), 'Delays [ms]':int(read(sr_tester))}
+            df = df.append(new_row, ignore_index=True)
+            time.sleep(random.randrange(0,5,1))
+        writeln(sr_tester, str(1<<STOP_CH_NR))
+
+Signal_processing_thread.join()
 result = tabulate(df, df.columns)
 print(result)
 shutil.rmtree('Results', ignore_errors=True)
