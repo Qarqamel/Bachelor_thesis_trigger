abbe2d1a9e1a7086edacab8125288a8fc932b9ac
Adam Mundała <88681221+Qarqamel@users.noreply.github.com>
2021-11-27 14:02:59 +0100

    TB_meter_period

---
+++ TB_meter_period/TB_meter_period_0.0625us.py
@@ -0,0 +1,44 @@
+SAMPLE_NR = 2900
+METER_UNIT_IN_US = 0.0625
+PERIOD_INPUT_US = 25
+
+METER_COM_NR = 15
+
+from tabulate import tabulate
+import shutil, os, sys, pickle
+import pandas as pd
+from collections import Counter
+sys.path.append('../')
+from my_serial import my_serial,read,writeln
+
+with my_serial(METER_COM_NR) as sr_meter:
+    print("sr_meter:"+read(sr_meter))
+    print("sr_meter:"+read(sr_meter))
+    
+    writeln(sr_meter, 'start')
+    
+    df = pd.DataFrame()
+    df['Measured Period'] = [int(read(sr_meter)) for i in range(SAMPLE_NR)]
+    
+df['Measured Period [us]'] = df['Measured Period']#*METER_UNIT_IN_US
+df['Set Period [us]'] = PERIOD_INPUT_US
+df['Error [%]'] = ((df['Set Period [us]'] - df['Measured Period [us]'])/df['Set Period [us]'])*100
+df.drop(columns=['Measured Period'], inplace = True)              
+
+result = tabulate(df, df.columns)
+cnt_val = Counter(df['Measured Period [us]'])
+cnt_val = list(map(list, cnt_val.items()))
+cnt_val = tabulate(cnt_val, headers = ['period', 'nr'])             
+print(result)
+print(cnt_val)
+
+shutil.rmtree('Results', ignore_errors=True)
+os.mkdir('Results')                
+with open("Results/results.txt", 'w') as f:
+    f.write(result)
+with open("Results/summary.txt", 'w') as f:
+    f.write(cnt_val)
+with open("Results/results", "wb") as f:
+    pickle.dump(result, f)
+        
+
